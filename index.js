db4dabdffc29a228ec75c5d75689483c132128a1
Darrel Kathan <Darrel.Kathan@grainger.com>
2017-08-17 12:09:00 -0500

    bug fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var fs = require('fs'),
     url = require('url'),
     ignore = ['.DS_Store'],
+    Async = require('async'),
 FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
   if ( !(this instanceof FileAgent) ){return new FileAgent(in_fldr, out_fldrs, fld_fldr, fn, callback);}
   var in_folder,
@@ -13,20 +14,24 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
       self = this;
       
   this.setInFolder = function(in_fldr, cb){
-    var result, is_active = active;
+    var is_active = active;
     this.stop();
-    if(ensureFolder(in_fldr)){
-      in_folder = in_fldr;
-      result = true;
-    }
-    
-    if(is_active){
-      this.start();
-    }
-    cb(result);
+    ensureFolder(in_fldr, (result)=>{
+      if(result){
+        in_folder = in_fldr;
+        result = true;
+      }
+      if(is_active){
+        return this.start((re) => {
+          cb(re);
+        });
+      }
+      cb(result);
+    });
   }
   
   this.setOutFolders = function(out_fldrs, cb){
+    //console.log('setting out folders...');
     if(Array.isArray(out_fldrs)){
       out_fldrs.forEach((out_fldr, i) => {
         ensureFolder(out_fldr, (result)=>{
@@ -45,56 +50,66 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
     ensureFolder(fld_fldr, (result)=>{
       if(result){
         failed_folder = fld_fldr;
-        return cb(true);
       }
-      return cb(false);
+      return cb(result);
     });
   }
   
-  this.setFunction = function(fn){
+  this.setFunction = function(fn, cb){
     var is_active = active;
     this.stop();
     func = fn;
     if(is_active){
-      this.start();
+      return this.start(()=>{
+        cb();
+      });
     }
+    return cb();
   }
   
   function ensureFolder(path, cb){
-    //console.log('exists', path);
+    
     if(!fs.existsSync(path)){
       var path_ary = path.split('/'),
           cur_path = '/',
           p = 0;
       
       path_ary.forEach((cur_fldr, i)=>{
-        cur_path += cur_fldr+'/';
-        if(!fs.existsSync(cur_path)){
-          fs.mkdir(cur_path, (result)=>{
-            p++;
-            if(!result){
-              return cb(false);
-            }
-            console.log('created', cur_path);
-            if(p === path_ary.length) {
-              cb(false);
-            }
-          }); 
-        }else{
-          console.log(cur_path, 'exists');
+        if(cur_fldr !== ''){
+          cur_path += cur_fldr+'/';
+          
+          if(!fs.existsSync(cur_path)){
+            
+            fs.mkdir(cur_path, (err)=>{
+              p++;
+              if(err){
+                console.log('Error creating', cur_path, err);
+                return cb(false);
+              }
+              
+              if(p === path_ary.length) {
+                console.log('Created', cur_path);
+                cb(true);
+              }
+            }); 
+          }
         }
       });
+    }else{
+      console.log(path, 'exists');
+      cb(true);
     }
-    console.log(path, 'exists');
-    cb(true);
   }
   
   function wrapperFunction(eventType, filename){
-    var orig_path = url.resolve(in_folder, filename),
-        new_path = url.resolve(failed_folder, filename);
+    var orig_path = in_folder+ filename,
+        new_path = failed_folder+ filename;
+        
+    //console.log('wrapperFunction', eventType, filename, orig_path, fs.existsSync(orig_path));
     if(ignore.indexOf(filename) === -1 && fs.existsSync(orig_path)){
-      self.stop();
+      //self.stop();
       func(eventType, filename, in_folder, out_folders, failed_folder, (err, out)=>{
+        console.log('Found', orig_path);
         if(out && out_folders[out]){
           if(fs.existsSync(out_folders[out])){
             new_path = url.resolve(out_folders[out], filename);
@@ -104,7 +119,7 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
         if(fs.existsSync(orig_path)){
           console.log('Moving', orig_path, '->', new_path);
           fs.rename(orig_path, new_path, ()=>{
-            self.start();
+            //self.start(()=>{});
           });
         }
         //self.start();
@@ -113,10 +128,13 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
   }
   
   this.start = function(cb){
+    
     ensureFolder(in_folder, (result) => {
+      
       if(!active && fs.existsSync(in_folder)){
-        //console.log(in_folder);
+        //console.log('Setting watch on', in_folder);
         watcher = fs.watch(in_folder, wrapperFunction);
+        console.log('Watching', in_folder);
         active = true;
         return cb(true);
       }
@@ -133,41 +151,53 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
   //Scalar.call(this);//For subclass
   Object.assign(this, {});
   /* Constructor code */
-  var out = new Promise((resolve) => {
-    console.log('Setting out folder');
+  var out = (resolve, reject) => {
+    //console.log('Going to set out folders');
     this.setOutFolders(out_fldrs, (result)=>{
       if(result){
         resolve();
       }else{
-        callback('Could not ensure out folders.');
+        reject('Could not ensure out folders.');
       }
     });
-  }),
-  failed = new Promise((resolve) => {
+  },
+  failed = (resolve, reject) => {
+    //console.log('Going to set failed folder');
     this.setFailedFolder(fld_fldr, (result)=>{
       if(result){
         resolve();
       }else{
-        callback('Could not ensure failed folder.');
+        reject('Could not ensure failed folder.');
       }
     });
-  }),
-  inf = new Promise((resolve) => {
+  },
+  inf = (resolve, reject) => {
+    //console.log('Going to set in folder');
     this.setInFolder(in_fldr, (result)=>{
       if(result){
+        //console.log('Set in folder');
         resolve();
       }else{
-        callback('Could not ensure in folder.');
+        reject('Could not ensure in folder.');
       }
     });
-  }),
-  fun = new Promise((resolve)=>{
+  },
+  fun = (resolve)=>{
+    //console.log('Going to set function');
     this.setFunction(fn, ()=>{
+      //console.log('Set function');
       resolve();
     });
+  },
+  done = new Promise(()=>{
+    //console.log('done')
+    callback(null, self);
   });
-  Promise.all([out, failed, inf, fun]).then(()=>{
-    callback();
+  //Promise.all([out, failed, inf, fun]).then(
+  Async.parallel([out, failed, inf, fun],
+  (val)=>{
+    //console.log('done')
+    callback(null, self);
   });
 }
 
