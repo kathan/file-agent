d2cc020686aa79bc980a0fe00cec7e2d9c537ff9
Darrel Kathan <Darrel.Kathan@grainger.com>
2017-08-24 15:45:50 -0500

    isBeingWritten update

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+/*
+
+*/
 "use strict";
 var fs = require('fs'),
-    url = require('url'),
     ignore = ['.DS_Store', '.file-agent'],
     Async = require('async'),
     path = require('path'),
     execSync = require('child_process').execSync,
-FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
-  if ( !(this instanceof FileAgent) ){return new FileAgent(in_fldr, out_fldrs, fld_fldr, fn, callback);}
+FileAgent = function(opts, callback){
+  if ( !(this instanceof FileAgent) ){return new FileAgent(opts, callback);}
   var in_folder,
       out_folders = {},
       failed_folder,
       func,
       watcher,
       active = false,
-      self = this;
+      self = this,
+      filters;
       
   this.setInFolder = function(in_fldr, cb){
     if(typeof in_fldr === 'string'){
@@ -35,9 +38,9 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
         cb();
       });
     }else{
-      cb('In folder parameter must be an object.');
+      cb('setInFolder parameter must be a string. Received a '+typeof in_fldr);
     }
-  }
+  };
   
   this.setOutFolders = function(out_fldrs, cb){
     if(typeof out_fldrs === 'object'){
@@ -57,12 +60,17 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
         return cb(err);
       });
     }else{
-      return cb('Out folders parameter must be an object.');
+      return cb('setOutFolders parameter must be an object. Received a '+typeof out_fldrs);
     }
-  }
+  };
+  
+  this.setFilters = function (fil, cb){
+    filters = fil;
+    cb();
+  };
   
   this.setFailedFolder = function(fld_fldr, cb){
-    if(typeof in_fldr === 'string'){
+    if(typeof fld_fldr === 'string'){
       ensureFolder(fld_fldr, (err)=>{
         if(!err){
           failed_folder = fld_fldr;
@@ -73,9 +81,9 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
         return cb(err);
       });
     }else{
-      cb('Failed folder parameter must be an object.');
+      cb('setFailedFolder parameter must be a string. Received a '+typeof fld_fldr);
     }
-  }
+  };
   
   this.setFunction = function(fn, cb){
     
@@ -90,9 +98,9 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
       }
       return cb();
     }else{
-      return cb('Parameter must be a type function.');
+      return cb('setFunction parameter must be a function.');
     }
-  }
+  };
   
   function ensureFolder(path, cb){
     if(path){
@@ -150,56 +158,75 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
   function wrapperFunction(eventType, filename){
     var orig_path = in_folder+ filename,
         new_path = failed_folder+ filename;
-    console.log(orig_path, 1, eventType);
+    console.log(new Date(), 'wrapperFunction', orig_path, eventType);
     if(ignore.indexOf(filename) === -1 && fs.existsSync(orig_path)){
-      var start = new Date();
-      isBeingWritten(orig_path, (result) => {
-        console.log(orig_path, 3, (new Date()-start));
-        func(eventType, filename, in_folder, out_folders, failed_folder, (out)=>{
-          var d = new Date();
-          console.log(d, 'Found', orig_path);
-          if(out && out_folders[out] && fs.existsSync(out_folders[out])){
-            new_path = out_folders[out] + filename;
-          }
-          
-          if(fs.existsSync(orig_path)){
-            var d = new Date();
-            console.log(d, 'Moving', orig_path, '->', new_path);
-            fs.rename(orig_path, new_path, ()=>{});
-          }
-        });
+      filterCheck(orig_path, (result)=>{
+        if(result){
+          setImmediate(()=>{
+            isBeingWritten(orig_path, (result) => {
+              //console.log(new Date(), orig_path, result);
+              if(!result){
+                
+                console.log(new Date(), 'Found', orig_path);
+                func(eventType, filename, in_folder, out_folders, failed_folder, (out)=>{
+                  var d = new Date();
+                  
+                  if(out && out_folders[out] && fs.existsSync(out_folders[out])){
+                    new_path = out_folders[out] + filename;
+                  }
+                  
+                  if(fs.existsSync(orig_path)){
+                    d = new Date();
+                    console.log(d, 'Moving', orig_path, '->', new_path);
+                    fs.rename(orig_path, new_path, ()=>{});
+                  }
+                });
+              }else{
+                console.log(new Date(), `${filename} is being written.`);
+              }
+            });
+          });
+        }else{
+          console.log(new Date(), `${filename} didn't match filter.`);
+        }
       });
+    }else{
+      //console.log(`Skipping ${filename}`);
     }
+  }
+  
+  function filterCheck(filepath, cb) {
+    /* Check extension */
+    var result = true;
+    console.log('Filter check', filters);
+    if(filters.extensions && Array.isArray(filters.extensions)){
+      console.log('Checking extensions', filters.extensions);
+      var ext = path.extname(filepath).replace(/^./, '');
+      console.log(filepath, ext);
+      if(filters.extensions.indexOf(ext) === -1){
+        result = false;
+      }
+    }
+    cb(result);
   }
   
   function isBeingWritten(filepath, cb){
-    fs.open(filepath, 'r+', (err) => {
-      console.log('fs.open error', err);
-      if(err){
-        cb(false);
-      }else{
-        cb(true);
-      }
-    });
-  }
-  
-  function isBeingWrittenlsof(filepath, cb){
-    var start = new Date(),
-        file = path.basename(filepath),
-        dir = path.dirname(filepath);
+    
+      //TODO - fix path
+      var cmd = `/usr/local/lib/node_modules/lsof-mac-fast/vendor/fast_lsof -w '${filepath}'`;
+      console.log(new Date(), 'isBeingWritten cmd', cmd);
+      try{
+        var stdout = execSync(cmd);
         
-    //console.log('lsof start', start);
-    var cmd = `/usr/sbin/lsof -c Finder +D "${dir}" | grep "${file}"`;
-    console.log('cmd', cmd);
-    try{
-      cb(execSync(cmd));/*, (err, stdout, stderr)=>{
-      var end = new Date();
-      console.log('lsof end', (end-start));
-      cb();
-    });*/
-    } catch (err) {
-      cb(err);
-    }
+        if(stdout){
+          console.log(new Date(), 'stdout', stdout);
+          cb(true);
+        }else{
+          cb(false);
+        }
+      }catch(err){
+        cb(false);
+      }
   }
   
   function processExisting(cb){
@@ -229,42 +256,162 @@ FileAgent = function(in_fldr, out_fldrs, fld_fldr, fn, callback){
       }
       return cb(err);
     });
-  }
+  };
   
   this.stop = function(){
     if(active){
       watcher = fs.watch(in_folder);
       active = false;
     }
-  }
+  };
   
   Object.assign(this, {});
   /* Constructor code */
-  var out = (done) => {
-    this.setOutFolders(out_fldrs, (err)=>{
+  var fns = [(done) => {
+    this.setInFolder(opts.in, (err)=>{
       done(err);
     });
-  },
-  failed = (done) => {
-    this.setFailedFolder(fld_fldr, (err)=>{
-      done(err);
+  }];
+  
+  if(opts.filters){
+    fns.push((done) => {
+      this.setFilters(opts.filters, (err)=>{
+        done(err);
+      });
     });
-  },
-  inf = (done) => {
-    this.setInFolder(in_fldr, (err)=>{
-      done(err);
+  }
+  
+  if(opts.failed){
+    fns.push((done) => {
+      this.setFailedFolder(opts.failed, (err)=>{
+        done(err);
+      });
     });
-  },
-  fun = (done)=>{
-    this.setFunction(fn, (err)=>{
-      done(err);
+  }
+  
+  if(opts.iterator){
+    fns.push((done)=>{
+      this.setFunction(opts.iterator, (err)=>{
+        done(err);
+      });
     });
-  };
-  Async.parallel([out, failed, inf, fun],
-  (err)=>{
-    callback(err, self);
-  });
-}
+  }
+  
+  if(opts.out){
+    fns.push((done) => {
+      this.setOutFolders(opts.out, (err)=>{
+        done(err);
+      });
+    });
+  }
+  
+  Async.parallel(fns,
+    (err)=>{
+      callback(err, self);
+    }
+  );
+};
 
 FileAgent.prototype.constructor = FileAgent;
 module.exports = FileAgent;
+
+/*function isBeingWrittenLsof(filepath, cb){
+    var start = new Date();
+
+
+        
+    if(checking === false){
+      var cmd = `sudo /usr/sbin/lsof -f -w -n -- "${filepath}"`;
+      console.log(new Date(), 'cmd', cmd);
+      checking = true;
+      exec(cmd, (err, stdout, stderr)=>{
+        checking = false;
+        if(err){console.log(new Date(), 'lsof error', err); return cb(true);}
+        var end = new Date();
+        console.log(new Date(), 'lsof end', stdout, (end-start));
+        if(stdout){
+          cb(true);
+        }else{
+          cb(false);
+        }
+      });
+    }else{
+      cb(true);
+    }
+  }
+  function isBeingWrittenOpened(filepath, cb){
+    var Opened = require('@ronomon/opened');
+    var paths = [path.dirname(filepath)];
+    Opened.files(paths,
+      (error, hashTable) => {
+        if (error) throw error;
+        paths.forEach(
+          (path) => {
+            console.log(new Date(), path + ' open=' + hashTable.hasOwnProperty(path));
+          }
+        );
+        cb();
+      }
+    );
+  }
+  
+  function isBeingWrittenOpen(filepath, cb){
+  
+    fs.open(filepath, 'r+', (err, fd) => {
+      console.log(new Date(), 'fs.open error', err);
+      if(err){
+        cb(true);
+      }else{
+        cb(false);
+      }
+    });
+  }         
+  
+  function isBeingWrittenLsofSync(filepath, cb){
+    var file = path.basename(filepath),
+        dir = path.dirname(filepath);
+        
+    //console.log('lsof start', start);
+    //var cmd = `/usr/sbin/lsof -F n -- "${dir}" | grep "${file}"`;
+    if(checking === false){
+      checking = true;
+      var cmd = `/usr/sbin/lsof -F -P -l -M +D "${dir}" | grep "${file}"`;
+      console.log(new Date(), 'cmd', cmd);
+      
+      try{
+        
+        var re = execSync(cmd);
+        console.log(new Date(), 'execSync result', `${re}`);
+        checking = false;
+        cb(true);
+      } catch (err) {
+        checking = false;
+        cb(false);
+      }
+    }else{
+      cb(true);
+    }
+  }
+  
+  function isBeingWrittenFuser(filepath, cb){
+    var start = new Date();
+    if(checking === false){
+      //console.log('lsof start', start);
+      //var cmd = `/usr/sbin/lsof -F n -- "${dir}" | grep "${file}"`;
+      var cmd = `/usr/bin/fuser -u "${filepath}"`;
+      console.log(new Date(), 'cmd', cmd);
+      checking = true;
+      exec(cmd, (err, stdout, stderr)=>{
+        checking  = false;
+        var end = new Date();
+        console.log(new Date(), 'fuser end', stdout, (end-start));
+        if(stdout){
+          cb(true);
+        }else{
+          cb(false);
+        }
+      });
+    }else{
+      cb(true);
+    }
+  }*/
